397e0093ef5f7c354e90330f98391027dfeb400b
virginia-njeri <virginiaanjeri@gmail.com>
2022-03-01 23:04:25 +0300

    third commit

---
+++ src/main.kt
@@ -0,0 +1,21 @@
+fun main() {
+  var name="Akirachix"
+  var A=name[0].toString()+name[2]+name[3]
+  println(A)
+  var x="Virginia"
+  var y=24
+  var fullDetail="hi my name is $x and I am $y years old"
+  println(fullDetail)
+  var txt="kakuma"
+  println(txt.length)
+
+  var a="Stensil"
+  if (a=="Stensil"){
+println("that's me")}
+  else{
+    println("I don’t know who that is")}
+
+
+
+
+}
